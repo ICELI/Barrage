b7007dd702f66e8f7a56d27e8a10ab6c69f6cb51
郑斌 <wb-zhengbin@alipay.com>
2015-03-11 16:36:47 +0800

    插件版

--- dist/barrage.js
+++ dist/barrage.js
@@ -8,10 +8,8 @@
             colors: ['#3689c3', '#b96121', '#a8506b', '#e73357'], // 字体颜色
             rows: 2, // 行数
             maxRow: 0, // 最大宽度
-            elements: {
-                wrap: $('.barrage-wrap ul'),
-                item: $('.barrage-wrap li')
-            }
+            wrap: '.barrage-wrap',
+            top: 0
         };
 
         this.options = $.extend(true, {}, opts, options);
@@ -28,14 +26,21 @@
             });
             this.render();
             this.run();
+
+            return this;
         },
         render: function(){
             var that = this,
                 o = that.options;
 
-            o.elements.item.each(function(){
-                var me = $(this),
-                    index = me.index() % that.rows.length,
+            that.ul = $('<ul/>');
+            $(o.wrap).append(that.ul);
+
+            $(o.data).each(function(i, item){
+                var me = $('<li>' + this + '</li>');
+                    me.appendTo(that.ul);
+
+                var index = i % that.rows.length,
                     left = that.rows[index] + Math.floor(Math.random() * 150);
 
                 me.css({
@@ -46,10 +51,12 @@
 
                 that.rows[index] = left + me.width() + 20;
             });
+
+
             // 取最宽的一行
             that.maxRow = Math.max.apply(null, that.rows);
             // 设置为外部容器的宽度
-            o.elements.wrap.css({
+            that.ul.css({
                 'width': that.maxRow
             });
         },
@@ -57,13 +64,21 @@
             var that = this;
             var movePx = $(window).width();
             var timer = setInterval(function(){
-                if(movePx <= -that.options.elements.wrap.width()) {
+                if(movePx <= -that.ul.width()) {
                     clearInterval(timer);
                 }
-                that.options.elements.wrap.css('-webkit-transform',  'translate3d('+ (movePx -= 1.6) + 'px, 0, 0px)');
-            },10);
+                that.ul.css('-webkit-transform',  'translate3d('+ (movePx -= 1.6) + 'px, 0, 0px)');
+            }, that.options.speed + (Math.random() * that.options.rows));
         }
     };
 
     window.Barrage = Barrage;
+
+    $.fn = $.extend($.fn,{
+        barrage: function(opt){
+            $(this).each(function(){
+               this.Barrage = new Barrage($.extend(opt,{wrap: $(this)}));
+            });
+        }
+    });
 })(Zepto);
